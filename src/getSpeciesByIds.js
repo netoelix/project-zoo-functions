9aa1448c4e6d8ff04b444265bba98ba5cf3b3824
Elix Merlino Neto <neto.elix@outlook.com>
2023-08-26 14:16:21 -0300

    Finalizando o requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
-const getSpeciesByIds = (ids) => {
-  // seu código aqui
+const getSpeciesByIds = (...ids) => {
+  if (ids === undefined) {
+    return [];
+  }
+  const zooAnimals = data.species.filter((element) => ids.includes(element.id));
+
+  return zooAnimals;
 };
 
 module.exports = getSpeciesByIds;
