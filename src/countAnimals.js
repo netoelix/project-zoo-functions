0c781f10312a3b5aed1104d7f32e6532fb7022f6
Elix Merlino Neto <neto.elix@outlook.com>
2023-08-26 18:25:41 -0300

    Finalizando requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,28 @@
 const data = require('../data/zoo_data');
 
-const countAnimals = (animal) => {
-  // seu código aqui
+const countAnimals = (options = {}) => {
+  if (options.species) {
+    const species = data.species.find((species) => species.name === options.species);
+    if (species) {
+      if (options.sex) {
+        const filteredResidents = species.residents.filter((resident) => resident.sex === options.sex);
+        return filteredResidents.length;
+      }
+      return species.residents.length;
+    }
+    return 0;
+  }
+  const animalCounts = {};
+
+  for (const species of data.species) {
+    animalCounts[species.name] = species.residents.length;
+  }
+
+  return animalCounts;
 };
+
+module.exports = countAnimals;
+
+console.log(countAnimals('lions', 'male'));
 
 module.exports = countAnimals;
