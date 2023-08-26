eb20cca1d4599c196a52b5ae3e1a403154005bd2
Elix Merlino Neto <neto.elix@outlook.com>
2023-08-26 15:27:45 -0300

    Finalizando requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
 const getEmployeeByName = (employeeName) => {
-  // seu código aqui
+  if (employeeName === undefined) {
+    return {};
+  }
+  const zooEmployess = data.employees.find((element) =>
+    employeeName.includes(element.firstName) || employeeName.includes(element.lastName));
+  return zooEmployess;
 };
+console.log(getEmployeeByName('Nigel'));
 
 module.exports = getEmployeeByName;
